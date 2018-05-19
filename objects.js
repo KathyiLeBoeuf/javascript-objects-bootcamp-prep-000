c1ec39133a410814ab24ffd553f596b9e29b2dc6
Kathy <kleboeuf@townisp.com>
2018-05-19 21:31:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = { artistName: 'songTitle' };
 //console.log(playlist);
 
 function updatePlayList(playlist, artistName, songTitle) {
-   return Object.assign({}, playlist, {artistName: songTitle})
+   return Object.assign(playlist, {artistName: songTitle})
 }
     
 //console.log(updatePlayList(playlist, 'Bonzo', 'Can-Can'))
